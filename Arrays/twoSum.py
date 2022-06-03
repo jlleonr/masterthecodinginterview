f948f73870a283cf8b9ead28f2ef1c2f31a4ec82
Jose Leon <jl_leonr@yahoo.com>
2022-06-03 00:12:15 -0500

    Fixed flake8 issues

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -5,12 +5,14 @@ from typing import List, Tuple
 Given an array of integers, return the indices of the two numbers
 that add up to a given target
 '''
+
+
 def sumTwo(nums: List[int], target: int) -> Tuple[int, int]:
-# Check if the complement of the value exist in a hash map
-# If the value does not exist, add the complement to the hash map
-# Add the value as a key to a dictionary and the index as the value
-# If we find the comp in the map, return the current index and the
-# value of dict[current_value] which will give the second index
+    # Check if the complement of the value exist in a hash map
+    # If the value does not exist, add the complement to the hash map
+    # Add the value as a key to a dictionary and the index as the value
+    # If we find the comp in the map, return the current index and the
+    # value of dict[current_value] which will give the second index
 
     if len(nums) < 2:
         return None
@@ -25,7 +27,8 @@ def sumTwo(nums: List[int], target: int) -> Tuple[int, int]:
 
     return None
 
+
 testNums: List[int] = [3, 2, 1, 4]
 testTarget: int = 7
 
-print(sumTwo(nums= testNums, target= testTarget))
+print(sumTwo(nums=testNums, target=testTarget))
