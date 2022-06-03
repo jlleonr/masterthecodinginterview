04f67218a5a7dc72e399a4a342d8feb1428d5020
Jose Leon <jl_leonr@yahoo.com>
2022-06-02 23:53:14 -0500

    Updated comments

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -6,11 +6,11 @@ Given an array of integers, return the indices of the two numbers
 that add up to a given target
 '''
 def sumTwo(nums: List[int], target: int) -> Tuple[int, int]:
-# Check if the complement of the value exist in a hash set
-# If the value does not exist, add the complement to the set
+# Check if the complement of the value exist in a hash map
+# If the value does not exist, add the complement to the hash map
 # Add the value as a key to a dictionary and the index as the value
-# If we find the comp in the set, return the current index and the
-# value of dict[target - value] which will give the second index
+# If we find the comp in the map, return the current index and the
+# value of dict[current_value] which will give the second index
 
     if len(nums) < 2:
         return None
